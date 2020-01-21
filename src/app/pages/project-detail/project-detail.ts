74acc7fc0eb1294487336ab1ba44ffc0f4621f71
nicoLepaj <nicolas.lepajolec@gmail.com>
2020-01-21 12:37:53 +0100

    fix upload multiple project images

--- src/app/pages/project-detail/project-detail.ts
+++ src/app/pages/project-detail/project-detail.ts
@@ -1,68 +1,63 @@
-import { Component, OnInit } from "@angular/core";
-import { ProjectData } from "src/app/services/project-data";
-import { ActivatedRoute } from "@angular/router";
+import { Component, OnInit, ɵConsole } from '@angular/core';
+import { ProjectData } from 'src/app/services/project-data';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: "project-detail",
-  templateUrl: "./project-detail.html",
-  styleUrls: ["./project-detail.scss"]
+	selector: 'project-detail',
+	templateUrl: './project-detail.html',
+	styleUrls: ['./project-detail.scss']
 })
 export class ProjectDetail implements OnInit {
-  files = [];
-  project: any;
-  urls = [];
-  loading: boolean;
+	files = [];
+	project: any;
+	urls = [];
+	loading: boolean;
 
-  constructor(public projectData: ProjectData, public route: ActivatedRoute) {}
+	constructor(public projectData: ProjectData, public route: ActivatedRoute) {}
 
-  ngOnInit() {
-    this.getProject();
-  }
+	ngOnInit() {
+		this.getProject();
+	}
 
-  async getProject() {
-    this.loading = true;
-    const id = this.route.snapshot.paramMap.get("id");
-    const project = await this.projectData.getProjectById(id);
-    this.project = project.data();
-    this.project.id = id;
-    this.getImages().then(() => {
-      this.loading = false;
-    });
-  }
+	async getProject() {
+		this.loading = true;
+		const id = this.route.snapshot.paramMap.get('id');
+		const project = await this.projectData.getProjectById(id);
+		this.project = project.data();
+		this.project.id = id;
+		this.getImages().then(() => {
+			this.loading = false;
+		});
+	}
 
-  resetInput(inputId) {
-    let fileInput = document.getElementById(inputId) as HTMLInputElement;
-    fileInput.value = "";
-    this.files = [];
-  }
+	resetInput(inputId) {
+		let fileInput = document.getElementById(inputId) as HTMLInputElement;
+		fileInput.value = '';
+		this.files = [];
+	}
 
-    selectFile(event) {
-    Object.keys(event.srcElement.files).forEach(key => {
-      const value = event.srcElement.files[key];
-      this.files.push(value);
-    });
-    
-    setTimeout(() => {
-      this.saveImages();
-    }, 2000);
-  }
+	async selectFile(event) {
+		for (const key of Object.keys(event.srcElement.files)) {
+			const value = await event.srcElement.files[key];
+			this.files.push(value);
+		}
+		this.saveImages();
+	}
 
-  
+	async saveImages() {
+		console.log('saving the images');
+		try {
+			await this.projectData.saveImages(this.files, this.project.id);
+			this.getImages();
+			console.log('success');
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
-  async saveImages() {
-    console.log("saving the images");
-    try {
-      await this.projectData.saveImages(this.files, this.project.id);
-      this.getImages();
-      console.log("success");
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  async getImages() {
-    this.urls = [];
-    const url = await this.projectData.getImages(this.project.id);
-    this.urls = url;
-  }
+	async getImages() {
+		this.urls = [];
+		const url = await this.projectData.getImages(this.project.id);
+		this.urls = url;
+	}
 }
