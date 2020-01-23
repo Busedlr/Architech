d134ee710198cc0591a351bc02f312f3fcb6c3c9
nicoLepaj <nicolas.lepajolec@gmail.com>
2020-01-23 12:54:24 +0100

    fix getting download urls

--- src/app/pages/project-detail/project-detail.ts
+++ src/app/pages/project-detail/project-detail.ts
@@ -1,97 +1,111 @@
-import { Component, OnInit, ɵConsole } from "@angular/core";
-import { ProjectData } from "src/app/services/project-data";
-import { ActivatedRoute } from "@angular/router";
-import { ImageDisplayModalPage } from "src/app/modals/image-display-modal/image-display-modal.page";
-import { ModalController } from "@ionic/angular";
+import { Component, OnInit, ɵConsole } from '@angular/core';
+import { ProjectData } from 'src/app/services/project-data';
+import { ActivatedRoute } from '@angular/router';
+import { ImageDisplayModalPage } from 'src/app/modals/image-display-modal/image-display-modal.page';
+import { ModalController } from '@ionic/angular';
 
 @Component({
-  selector: "project-detail",
-  templateUrl: "./project-detail.html",
-  styleUrls: ["./project-detail.scss"]
+	selector: 'project-detail',
+	templateUrl: './project-detail.html',
+	styleUrls: ['./project-detail.scss']
 })
 export class ProjectDetail implements OnInit {
-  files = [];
-  project: any;
-  images = [];
-  loading: boolean;
-  editImages: boolean = false;
+	files = [];
+	project: any;
+	images = [];
+	loading: boolean;
+	editImages: boolean = false;
 
-  constructor(
-    public projectData: ProjectData,
-    public route: ActivatedRoute,
-    public modalController: ModalController
-  ) {}
+	constructor(
+		public projectData: ProjectData,
+		public route: ActivatedRoute,
+		public modalController: ModalController
+	) {}
 
-  ngOnInit() {
-    this.getProject();
-  }
+	ngOnInit() {
+		this.getProject();
+	}
 
-  async getProject() {
-    this.loading = true;
-    const id = this.route.snapshot.paramMap.get("id");
-    const project = await this.projectData.getProjectById(id);
-    this.project = project.data();
-    this.project.id = id;
-    this.getImages().then(() => {
-      this.loading = false;
-    });
-  }
+	async getProject() {
+		this.loading = true;
+		const id = this.route.snapshot.paramMap.get('id');
+		const project = await this.projectData.getProjectById(id);
+		this.project = project.data();
+		this.project.id = id;
+		this.getImages().then(() => {
+			this.loading = false;
+		});
+	}
 
-  resetInput(inputId) {
-    let fileInput = document.getElementById(inputId) as HTMLInputElement;
-    fileInput.value = "";
-    this.files = [];
-  }
+	resetInput(inputId) {
+		let fileInput = document.getElementById(inputId) as HTMLInputElement;
+		fileInput.value = '';
+		this.files = [];
+	}
 
-  async selectFile(event) {
-    for (const key of Object.keys(event.srcElement.files)) {
-      const value = await event.srcElement.files[key];
-      this.files.push(value);
-    }
-    console.log("images to be saved", this.files);
-    this.saveImages();
-  }
+	async selectFile(event) {
+		for (const key of Object.keys(event.srcElement.files)) {
+			const value = await event.srcElement.files[key];
+			this.files.push(value);
+		}
+		console.log('images to be saved', this.files);
+		this.saveImages();
+	}
 
-  async saveImages() {
-    console.log("saving the images");
-    try {
-      await this.projectData.saveImages(this.files, this.project.id);
-      this.getImages();
-      console.log("success");
-    } catch (error) {
-      console.log(error);
-    }
-  }
+	async saveImages() {
+		console.log('saving the images');
+		try {
+			await this.projectData.saveImages(this.files, this.project.id);
+			this.getImages();
+			console.log('success');
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
-  async getImages() {
+	/*   async getImages() {
     this.images = [];
     const storedImages = await this.projectData.getImages(this.project.id);
 	this.images = storedImages;
 	console.log(this.images)
-  }
+  } */
 
-  imageClick(i) {
-    if (this.editImages) {
-	  const clickedImage = document.getElementById(i) as HTMLInputElement;
-	  clickedImage.checked = !clickedImage.checked
-    } else this.openModal(i);
-  }
+	async getImages() {
+		this.images = [];
+		const imagesRef = await this.projectData.getImages(this.project.id);
+		for (const imageRef of imagesRef.items) {
+			const url = await this.projectData.getImageDownloadUrl(imageRef.fullPath);
+			const image = {
+				name: imageRef.name,
+				url: url
+			};
+			this.images.push(image);
+		}
+		console.log('images', this.images);
+	}
 
-  async openModal(i) {
-    const modal = await this.modalController.create({
-      component: ImageDisplayModalPage,
-      componentProps: {
-        index: i,
-        images: this.images
-      }
-    });
-    console.log(i);
-    return await modal.present();
-  }
+	imageClick(i) {
+		if (this.editImages) {
+			const clickedImage = document.getElementById(i) as HTMLInputElement;
+			clickedImage.checked = !clickedImage.checked;
+		} else this.openModal(i);
+	}
 
-  dismiss() {
-    this.modalController.dismiss({
-      dismissed: true
-    });
-  }
+	async openModal(i) {
+		const modal = await this.modalController.create({
+			component: ImageDisplayModalPage,
+			componentProps: {
+				index: i,
+				images: this.images
+			}
+		});
+		console.log(i);
+		return await modal.present();
+	}
+
+	dismiss() {
+		this.modalController.dismiss({
+			dismissed: true
+		});
+	}
 }
--- src/app/services/project-data.ts
+++ src/app/services/project-data.ts
@@ -1,110 +1,113 @@
-import { Injectable } from "@angular/core";
-import * as firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/storage";
-import { UrlSegment } from "@angular/router";
+import { Injectable } from '@angular/core';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/storage';
+import { UrlSegment } from '@angular/router';
 
 @Injectable({
-  providedIn: "root"
+	providedIn: 'root'
 })
 export class ProjectData {
-  db: any;
-  projectsRef: any;
-  storageRef: any;
+	db: any;
+	projectsRef: any;
+	storageRef: any;
 
-  constructor() {
-    this.db = firebase.firestore();
-    this.projectsRef = this.db.collection("projects");
-    this.storageRef = firebase.storage().ref();
-  }
+	constructor() {
+		this.db = firebase.firestore();
+		this.projectsRef = this.db.collection('projects');
+		this.storageRef = firebase.storage().ref();
+	}
 
-  saveProject(projectData) {
-    return this.projectsRef
-      .add(projectData)
-      .then(doc => {
-        return doc;
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
+	saveProject(projectData) {
+		return this.projectsRef
+			.add(projectData)
+			.then(doc => {
+				return doc;
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	}
 
-  getProjects() {
-    return this.projectsRef
-      .get()
-      .then(result => {
-        return result;
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
+	getProjects() {
+		return this.projectsRef
+			.get()
+			.then(result => {
+				return result;
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	}
 
-  getProjectById(id) {
-    return this.projectsRef
-      .doc(id)
-      .get()
-      .then(result => {
-        return result;
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
+	getProjectById(id) {
+		return this.projectsRef
+			.doc(id)
+			.get()
+			.then(result => {
+				return result;
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	}
 
-  saveImages(files, id) {
-    const rawFiles = [];
-    files.forEach(file => {
-      const promise = this.storageRef
-        .child(id + "/" + file.lastModified)
-        .put(file)
-        .catch(error => {
-          console.log(error);
-        });
-      rawFiles.push(promise);
-    });
-    return Promise.all(rawFiles);
-  }
+	saveImages(files, id) {
+		const rawFiles = [];
+		files.forEach(file => {
+			const promise = this.storageRef
+				.child(id + '/' + file.lastModified)
+				.put(file)
+				.catch(error => {
+					console.log(error);
+				});
+			rawFiles.push(promise);
+		});
+		return Promise.all(rawFiles);
+	}
 
-  /* saveImages(files) {
-    const rawFiles = [];
-    files.forEach(file => {
-      const promise = this.storageRef
-        .child("projectImages/" + file.lastModified)
-        .put(file)
-        .then(result => {
-          return result
-        })
-        .catch(error => {
-          console.log(error);
-        });
-      rawFiles.push(promise);
-    });
-    return Promise.all(rawFiles);
-  } // this one is without async await and above is the same code with async */
+	getImages1(id) {
+		return firebase
+			.storage()
+			.ref(id)
+			.listAll()
+			.then(result => {
+				const rawFiles = [];
+				result.items.forEach(imageRef => {
+					const promise = {
+						url: this.storageRef.child(imageRef.fullPath).getDownloadURL(),
+						name: imageRef.name
+					};
+					rawFiles.push(promise);
+				});
+				return Promise.all(rawFiles);
+			});
+	}
 
-  getImages(id) {
-    return firebase
-      .storage()
-      .ref(id)
-      .listAll()
-      .then(result => {
-        const rawFiles = [];
-        result.items.forEach(imageRef => {
-          const promise = {
-            url: this.storageRef.child(imageRef.fullPath).getDownloadURL(),
-            name: imageRef.name
-          };
-          rawFiles.push(promise);
-        });
-        return Promise.all(rawFiles);
-      });
-  }
+	getImages(id) {
+		return firebase
+			.storage()
+			.ref(id)
+			.listAll()
+			.then(res => {
+				console.log('res', res);
+				return res;
+			});
+	}
 
-  updateProjectData(imageUrl, id) {
-    this.db
-      .collection("projects")
-      .doc(id)
-      .update({ thumbnail: imageUrl });
-  }
+	getImageDownloadUrl(fullPath) {
+		return this.storageRef
+			.child(fullPath)
+			.getDownloadURL()
+			.then(res => {
+				return res;
+			});
+	}
+
+	updateProjectData(imageUrl, id) {
+		this.db
+			.collection('projects')
+			.doc(id)
+			.update({ thumbnail: imageUrl });
+	}
 }
