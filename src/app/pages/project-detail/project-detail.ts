26435dacd408ebf35a897326bc45780be95d9fa8
nicoLepaj <nicolas.lepajolec@gmail.com>
2020-01-23 19:42:00 +0100

    deleting images

--- src/app/pages/project-detail/project-detail.ts
+++ src/app/pages/project-detail/project-detail.ts
@@ -1,123 +1,136 @@
-import { Component, OnInit, ɵConsole } from "@angular/core";
-import { ProjectData } from "src/app/services/project-data";
-import { ActivatedRoute } from "@angular/router";
-import { ImageDisplayModalPage } from "src/app/modals/image-display-modal/image-display-modal.page";
-import { ModalController } from "@ionic/angular";
+import { Component, OnInit, ɵConsole } from '@angular/core';
+import { ProjectData } from 'src/app/services/project-data';
+import { ActivatedRoute } from '@angular/router';
+import { ImageDisplayModalPage } from 'src/app/modals/image-display-modal/image-display-modal.page';
+import { ModalController } from '@ionic/angular';
 
 @Component({
-  selector: "project-detail",
-  templateUrl: "./project-detail.html",
-  styleUrls: ["./project-detail.scss"]
+	selector: 'project-detail',
+	templateUrl: './project-detail.html',
+	styleUrls: ['./project-detail.scss']
 })
 export class ProjectDetail implements OnInit {
-  files = [];
-  project: any;
-  images = [];
-  loading: boolean;
-  editImages: boolean = false;
-  checkedImages: any = [];
+	files = [];
+	project: any;
+	images = [];
+	loading: boolean;
+	editImages: boolean = false;
+	checkedImages: any = [];
 
-  constructor(
-    public projectData: ProjectData,
-    public route: ActivatedRoute,
-    public modalController: ModalController
-  ) {}
+	constructor(
+		public projectData: ProjectData,
+		public route: ActivatedRoute,
+		public modalController: ModalController
+	) {}
 
-  ngOnInit() {
-    this.getProject();
-  }
+	ngOnInit() {
+		this.getProject();
+	}
 
-  async getProject() {
-    this.loading = true;
-    const id = this.route.snapshot.paramMap.get("id");
-    const project = await this.projectData.getProjectById(id);
-    this.project = project.data();
-    this.project.id = id;
-    this.getImages().then(() => {
-      this.loading = false;
-    });
-  }
+	async getProject() {
+		this.loading = true;
+		const id = this.route.snapshot.paramMap.get('id');
+		const project = await this.projectData.getProjectById(id);
+		this.project = project.data();
+		this.project.id = id;
+		this.getImages().then(() => {
+			this.loading = false;
+		});
+	}
 
-  resetInput(inputId) {
-    let fileInput = document.getElementById(inputId) as HTMLInputElement;
-    fileInput.value = "";
-    this.files = [];
-  }
+	resetInput(inputId) {
+		let fileInput = document.getElementById(inputId) as HTMLInputElement;
+		fileInput.value = '';
+		this.files = [];
+	}
 
-  async selectFile(event) {
-    for (const key of Object.keys(event.srcElement.files)) {
-      const value = await event.srcElement.files[key];
-      this.files.push(value);
-    }
-    console.log("images to be saved", this.files);
-    this.saveImages();
-  }
+	async selectFile(event) {
+		for (const key of Object.keys(event.srcElement.files)) {
+			const value = await event.srcElement.files[key];
+			this.files.push(value);
+		}
+		this.saveImages();
+	}
 
-  async saveImages() {
-    console.log("saving the images");
-    try {
-      await this.projectData.saveImages(this.files, this.project.id);
-      this.getImages();
-      console.log("success");
-    } catch (error) {
-      console.log(error);
-    }
-  }
+	async saveImages() {
+		try {
+			await this.projectData.saveImages(this.files, this.project.id);
+			this.getImages();
+			console.log('success');
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
-  async getImages() {
-    this.images = [];
-    const imagesRef = await this.projectData.getImages(this.project.id);
-    for (const imageRef of imagesRef.items) {
-      const url = await this.projectData.getImageDownloadUrl(imageRef.fullPath);
-      const image = {
-        fullPath: imageRef.fullPath,
-        url: url
-      };
-      this.images.push(image);
-    }
-    console.log("images", this.images);
-  }
+	async getImages() {
+		this.images = [];
+		const imagesRef = await this.projectData.getImagesRef(this.project.id);
 
-  imageClick(i) {
-    if (this.editImages) {
-      const clickedImage = document.getElementById(i) as HTMLInputElement;
-      clickedImage.checked = !clickedImage.checked;
-      if (clickedImage.checked) {
-		  this.checkedImages = [];
-        this.checkedImages.push(clickedImage.id);
-      } else {
-        this.checkedImages.splice[(clickedImage.id, 1)];
-      }
-    } else this.openModal(i);
-  }
+		for (const imageRef of imagesRef.items) {
+			const url = await this.projectData.getImageDownloadUrl(imageRef.fullPath);
 
-  editImagesClick() {
-    this.editImages = !this.editImages;
-  }
+			const image = {
+				fullPath: imageRef.fullPath,
+				url: url
+			};
+			this.images.push(image);
+		}
+	}
 
-  async openModal(i) {
-    const modal = await this.modalController.create({
-      component: ImageDisplayModalPage,
-      componentProps: {
-        index: i,
-        images: this.images
-      }
-    });
-    console.log(i);
-    return await modal.present();
-  }
+	imageClick(i, image) {
+		if (this.editImages) {
+			const clickedImage = document.getElementById(i) as HTMLInputElement;
+			clickedImage.checked = !clickedImage.checked;
 
-  dismiss() {
-    this.modalController.dismiss({
-      dismissed: true
-    });
-  }
+			if (clickedImage.checked) {
+				this.checkedImages.push(image);
+			} else {
+				const removeIndex = this.checkedImages.findIndex(
+					obj => obj.name === image.name
+				);
 
-  async deleteImage() {
-    for (const i of this.checkedImages) {
-      let fullPath = this.images[i].fullPath;
-      await this.projectData.deleteImage(fullPath);
-    }
-    this.getImages();
-  }
+				this.checkedImages.splice(removeIndex, 1);
+			}
+		} else this.openModal(i);
+	}
+
+	toggleEditImages() {
+		this.checkedImages = [];
+		this.editImages = !this.editImages;
+		if (!this.editImages) this.resetCheckedImages();
+	}
+
+	resetCheckedImages() {
+		this.images.forEach((image, i) => {
+			const clickedImage = document.getElementById(
+				i.toString()
+			) as HTMLInputElement;
+			clickedImage.checked = false;
+		});
+	}
+
+	async openModal(i) {
+		const modal = await this.modalController.create({
+			component: ImageDisplayModalPage,
+			componentProps: {
+				index: i,
+				images: this.images
+			}
+		});
+		return await modal.present();
+	}
+
+	dismiss() {
+		this.modalController.dismiss({
+			dismissed: true
+		});
+	}
+
+	async deleteImages() {
+		for (const image of this.checkedImages) {
+			await this.projectData.deleteImage(image.fullPath);
+		}
+		this.toggleEditImages();
+		this.getImages();
+	}
 }
--- src/app/services/project-data.ts
+++ src/app/services/project-data.ts
@@ -1,126 +1,106 @@
-import { Injectable } from "@angular/core";
-import * as firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/storage";
+import { Injectable } from '@angular/core';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/storage';
 
 @Injectable({
-  providedIn: "root"
+	providedIn: 'root'
 })
 export class ProjectData {
-  db: any;
-  projectsRef: any;
-  storageRef: any;
+	db: any;
+	projectsRef: any;
+	storageRef: any;
 
-  constructor() {
-    this.db = firebase.firestore();
-    this.projectsRef = this.db.collection("projects");
-    this.storageRef = firebase.storage().ref();
-  }
+	constructor() {
+		this.db = firebase.firestore();
+		this.projectsRef = this.db.collection('projects');
+		this.storageRef = firebase.storage().ref();
+	}
 
-  saveProject(projectData) {
-    return this.projectsRef
-      .add(projectData)
-      .then(doc => {
-        return doc;
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
+	saveProject(projectData) {
+		return this.projectsRef
+			.add(projectData)
+			.then(doc => {
+				return doc;
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	}
 
-  getProjects() {
-    return this.projectsRef
-      .get()
-      .then(result => {
-        return result;
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
+	getProjects() {
+		return this.projectsRef
+			.get()
+			.then(result => {
+				return result;
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	}
 
-  getProjectById(id) {
-    return this.projectsRef
-      .doc(id)
-      .get()
-      .then(result => {
-        return result;
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
+	getProjectById(id) {
+		return this.projectsRef
+			.doc(id)
+			.get()
+			.then(result => {
+				return result;
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	}
 
-  saveImages(files, id) {
-    const rawFiles = [];
-    files.forEach(file => {
-      const promise = this.storageRef
-        .child(id + "/" + file.lastModified)
-        .put(file)
-        .catch(error => {
-          console.log(error);
-        });
-      rawFiles.push(promise);
-    });
-    return Promise.all(rawFiles);
-  }
+	saveImages(files, id) {
+		const rawFiles = [];
+		files.forEach(file => {
+			const promise = this.storageRef
+				.child(id + '/' + file.lastModified)
+				.put(file)
+				.catch(error => {
+					console.log(error);
+				});
+			rawFiles.push(promise);
+		});
+		return Promise.all(rawFiles);
+	}
 
-  getImages1(id) {
-    return firebase
-      .storage()
-      .ref(id)
-      .listAll()
-      .then(result => {
-        const rawFiles = [];
-        result.items.forEach(imageRef => {
-          const promise = {
-            url: this.storageRef.child(imageRef.fullPath).getDownloadURL(),
-            name: imageRef.name
-          };
-          rawFiles.push(promise);
-        });
-        return Promise.all(rawFiles);
-      });
-  }
+	getImagesRef(id) {
+		return firebase
+			.storage()
+			.ref(id)
+			.listAll()
+			.then(res => {
+				return res;
+			});
+	}
 
-  getImages(id) {
-    return firebase
-      .storage()
-      .ref(id)
-      .listAll()
-      .then(res => {
-        console.log("res", res);
-        return res;
-      });
-  }
+	getImageDownloadUrl(fullPath) {
+		return this.storageRef
+			.child(fullPath)
+			.getDownloadURL()
+			.then(res => {
+				return res;
+			});
+	}
 
-  getImageDownloadUrl(fullPath) {
-    return this.storageRef
-      .child(fullPath)
-      .getDownloadURL()
-      .then(res => {
-        return res;
-      });
-  }
+	updateProjectData(imageUrl, id) {
+		this.db
+			.collection('projects')
+			.doc(id)
+			.update({ thumbnail: imageUrl });
+	}
 
-  updateProjectData(imageUrl, id) {
-    this.db
-      .collection("projects")
-      .doc(id)
-      .update({ thumbnail: imageUrl });
-  }
-
-  deleteImage(fullPath) {
-    const imgToDelete = fullPath;
-    return this.storageRef
-      .child(imgToDelete)
-      .delete()
-      .then((result) => {
-		console.log("images are deleted");
-		return result
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
+	deleteImage(fullPath) {
+		const imgToDelete = fullPath;
+		return this.storageRef
+			.child(imgToDelete)
+			.delete()
+			.then(result => {
+				return result;
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	}
 }
