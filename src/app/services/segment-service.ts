f665eee5090f06a097a54598c6fff78c1cec84c5
Busedlr <b.dilara@live.com>
2020-03-25 12:23:29 +0100

    added segment service

---
+++ src/app/services/segment-service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+  providedIn: "root"
+})
+export class SegmentsService {
+  images: any[] = [];
+  documents: any[] = [];
+  companies: any[] = [];
+  constructor() {}
+
+
+  
+
+  
+}
